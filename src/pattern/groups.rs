b5a80fc1d0cc4b3027cce1f81e1e87250f77d82e
m4jkiuwr <m.ovczarz@gmail.com>
2025-08-11 00:38:51 +0200

    codecrafters submit [skip ci]

---
+++ src/pattern/groups.rs
@@ -0,0 +1,40 @@
+use super::PatternElems;
+use std::collections::HashSet;
+
+#[derive(Debug)]
+pub struct PositiveGroup(HashSet<char>);
+
+impl Into<PositiveGroup> for Vec<char> {
+    fn into(self) -> PositiveGroup {
+        let mut map = HashSet::new();
+        self.iter().for_each(|c| {
+            map.insert(*c);
+        });
+        PositiveGroup(map)
+    }
+}
+
+impl PatternElems for PositiveGroup {
+    fn matcher(self) -> Box<dyn Fn(&mut std::str::Chars) -> bool> {
+        Box::new(move |it| it.any(|c| self.0.contains(&c)))
+    }
+}
+
+#[derive(Debug)]
+pub struct NegativeGroup(HashSet<char>);
+
+impl Into<NegativeGroup> for Vec<char> {
+    fn into(self) -> NegativeGroup {
+        let mut map = HashSet::new();
+        self.iter().for_each(|c| {
+            map.insert(*c);
+        });
+        NegativeGroup(map)
+    }
+}
+
+impl PatternElems for NegativeGroup {
+    fn matcher(self) -> Box<dyn Fn(&mut std::str::Chars) -> bool> {
+        Box::new(move |it| it.any(|c| !self.0.contains(&c)))
+    }
+}
