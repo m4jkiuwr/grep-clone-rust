195e7c7131a06038c938c8132324f48060a3b689
m4jkiuwr <m.ovczarz@gmail.com>
2025-08-10 20:55:40 +0200

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub fn find_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         input_line.contains(pattern)
-    } else if pattern == r"\d" {
-        input_line.chars().any(|c| c.is_numeric())
     } else {
-        panic!("Unhandled pattern: {}", pattern)
+        match pattern {
+            r"\d" => input_line.chars().any(|c| c.is_numeric()),
+            r"\w" => input_line.chars().any(|c| c.is_alphanumeric() || c == '_'),
+            _ => todo!(),
+        }
     }
 }
