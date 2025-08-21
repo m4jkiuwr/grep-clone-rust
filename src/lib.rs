821f4895597b57568810d10533ebef4760a60b55
m4jkiuwr <m.ovczarz@gmail.com>
2025-08-21 17:39:53 +0200

    CLI tests (cli-test-1755790793829)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@ mod pattern;
 use pattern::ReAutomata;
 
 pub fn find_pattern(input_line: String, pattern: &str) -> bool {
-    let mut automata = ReAutomata::from(pattern);
-    automata.run(&input_line.chars().collect::<Vec<char>>())
+    let mut chars = pattern.chars();
+    let input: Vec<char> = input_line.chars().collect();
+    let mut automata = ReAutomata::from(&mut chars);
+    println!("{:#?}", automata);
+    let matches = automata.all_matches(&input);
+
+    let res = !matches.is_empty();
+
+    for (a, b) in matches {
+        println!("{} {}", a, b);
+    }
+
+    res // Return the actual result
 }
--- src/pattern/states/mod.rs
+++ src/pattern/states/mod.rs
@@ -1,12 +1,14 @@
 use super::State;
-use super::{ReHash, ReInput, ReOutput, ReRef};
+use super::{ReAutomata, ReHash, ReInput, ReMatch, ReRef};
 
 mod alphanumeric;
 mod digit;
 mod endanchor;
+mod group;
 mod literal;
 mod negativegroup;
 mod one_or_more;
+mod or_else;
 mod positivegroup;
 mod startanchor;
 mod wildcard;
@@ -15,9 +17,11 @@ mod zero_or_one;
 pub use alphanumeric::AlphaNum;
 pub use digit::Digit;
 pub use endanchor::EndAnchor;
+pub use group::Group;
 pub use literal::Literal;
 pub use negativegroup::NegativeGroup;
 pub use one_or_more::OneOrMore;
+pub use or_else::OrElse;
 pub use positivegroup::PositiveGroup;
 pub use startanchor::StartAnchor;
 pub use wildcard::Wildcard;
